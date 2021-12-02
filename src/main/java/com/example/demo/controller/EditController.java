69c9265bd4f12993a5f84f7d56c3bcb1f5b4bae5
Yuya Nagasaka <nagasaka.yuya0928@gmail.com>
2021-12-02 16:13:09 +0900

    [ADD]投稿削除機能の操作を実装

--- src/main/java/com/example/demo/controller/EditController.java
+++ src/main/java/com/example/demo/controller/EditController.java
@@ -1,5 +1,24 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.object.Output;
+import com.example.demo.service.OutputService;
+
+
+@Controller
 public class EditController {
+
+@Autowired OutputService service;
+@PostMapping(value = "/Edit",params = "delete")
+
+public String Delete(@ModelAttribute Output output) {
+	service.delete(output);
+	 return "redirect:/Index";  
+}
+
 
 }
