0f6407db093fbab58537a2475a0ecd1f404de714
Yuya Nagasaka <nagasaka.yuya0928@gmail.com>
2021-12-02 14:39:44 +0900

    [ADD]投稿一覧情報のModelへの格納機能を追記

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -1,13 +1,22 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.object.Output;
+import com.example.demo.service.OutputService;
 
 @Controller
 public class IndexController {
-  
+  @Autowired OutputService service;
   @GetMapping("/Index")
-  public String getIndex() {
+  public String getIndex(Model model) {
+	List<Output> outputIndex =  service.Index();
+	model.addAttribute("outputIndex",outputIndex);
     return "Index";
   }
   
