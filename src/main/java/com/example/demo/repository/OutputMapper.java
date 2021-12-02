6f7ffa64b95f23e70b23ce688b295fe2d5bb8733
Yuya Nagasaka <nagasaka.yuya0928@gmail.com>
2021-12-02 14:38:03 +0900

    [ADD]新規投稿・投稿一覧取得部分のインターフェイス追記

--- src/main/java/com/example/demo/repository/OutputMapper.java
+++ src/main/java/com/example/demo/repository/OutputMapper.java
@@ -1,5 +1,15 @@
 package com.example.demo.repository;
 
-public interface OutputMapper {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.demo.object.Output;
+
+@Mapper
+public interface OutputMapper {
+  
+  public void Create(Output output);
+  
+  public List<Output> Index();
 }
