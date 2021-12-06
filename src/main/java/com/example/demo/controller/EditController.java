6efcc7c13e4fca44ebcca0b254017bf6a9f3e689
Yuya Nagasaka <nagasaka.yuya0928@gmail.com>
2021-12-06 13:12:53 +0900

    [ADD]編集処理(コントローラー)追記

--- src/main/java/com/example/demo/controller/EditController.java
+++ src/main/java/com/example/demo/controller/EditController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -12,7 +14,15 @@ import com.example.demo.service.OutputService;
 @Controller
 public class EditController {
 
+
+	
 @Autowired OutputService service;
+
+@GetMapping("/Edit")
+public String getEdit() {
+  return "Edit";
+}
+
 @PostMapping(value = "/Edit",params = "delete")
 
 public String Delete(@ModelAttribute Output output) {
@@ -20,5 +30,10 @@ public String Delete(@ModelAttribute Output output) {
 	 return "redirect:/Index";  
 }
 
+@PostMapping(value = "/Edit",params = "edit")
 
+public String Update(@ModelAttribute Output output,Model model) {
+	model.addAttribute("output", service.edit(output));
+	return "Edit";  
+}
 }
