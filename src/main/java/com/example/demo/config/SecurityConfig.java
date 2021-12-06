491eb0b59c132cf3d66120c60eef905f742572c0
Yuya Nagasaka <nagasaka.yuya0928@gmail.com>
2021-12-06 15:12:28 +0900

    [ADD]直リンク禁止機能追加

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,5 +1,26 @@
 package com.example.demo.config;
 
-public class SecurityConfig {
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+     
+	
+	
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()//アクセス設定
+                .antMatchers("/Top").permitAll()//トップページ直接アクセス許可
+                .antMatchers("/Login").permitAll()//ログインページ直接アクセス許可
+                .antMatchers("/Register").permitAll()//会員登録ページ直接アクセス許可
+                .anyRequest().authenticated();//全パス認証必須
+        
+    }
 
 }
