a11df7f3612db55cf57ce82b6e1f2f0297ce0e88
Yuya Nagasaka <nagasaka.yuya0928@gmail.com>
2021-12-13 22:42:22 +0900

    [fix]ログアウト処理の追記

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -20,39 +20,44 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+	
 	@Autowired UserDetailsService userDetailsService;
 	
     @Override
     protected void configure(HttpSecurity http) throws Exception {
     	
         http
-            .authorizeRequests()//アクセス設定
+            .authorizeRequests()//アクセス許可設定
                 .antMatchers("/Top").permitAll()//トップページ直接アクセス許可
                 .antMatchers("/Login").permitAll()//ログインページ直接アクセス許可
                 .antMatchers("/Register").permitAll()//会員登録ページ直接アクセス許可
                 .anyRequest().authenticated();//全パス認証必須
         http
-            .formLogin()//ログイン処理
-                .loginProcessingUrl("/Login") 
-                .loginPage("/Login")
-                .failureUrl("/Login?error")
-                .usernameParameter("name")
+            .formLogin()//フォームの認証処理
+                .loginProcessingUrl("/Login") //認証のトリガー
+                .loginPage("/Login")//認証を行うページ
+                .failureUrl("/Login?error")//認証失敗時のページ
+                .usernameParameter("name")//認証に使用する情報
                 .passwordParameter("password")
-                .defaultSuccessUrl("/Create",true);
+                .defaultSuccessUrl("/Create",true);//成功時の遷移先
         
-        http.csrf().disable();
+        http
+            .logout()//ログアウト処理
+              .logoutUrl("/Logout")
+              .logoutSuccessUrl("/Top?Logout");
+        
     }
     
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-      PasswordEncoder encoder = passwordEncoder();
+      auth.userDetailsService(userDetailsService)//loadUserByUsername で取得した情報を元に認証
+      .passwordEncoder(passwordEncoder());
+      
       /*
       auth.inMemoryAuthentication()
       .withUser("nagasaka")
         .password(encoder.encode("nagasaka0928"))
         .roles("admin");
       */
-      auth.userDetailsService(userDetailsService)
-      .passwordEncoder(encoder);
     }
   }
