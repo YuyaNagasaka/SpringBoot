b639c2a72c6f633761fcbcb33e66b920f94234f5
Yuya Nagasaka <nagasaka.yuya0928@gmail.com>
2021-12-13 22:45:11 +0900

    [Fix]UserDetailクラスの修正(userdettails.user型の変数を作成 UserDetails 型変数にキャスト)

--- src/main/java/com/example/demo/service/UserDetailService.java
+++ src/main/java/com/example/demo/service/UserDetailService.java
@@ -9,22 +9,27 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserDetailService implements UserDetailsService {
 	@Autowired UserService service;
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    //loginProcessingUrl で指定されたページにPostリクエストが届いたタイミングで自動実行
+    //送信したユーザー情報が存在するか確認
+    public UserDetails loadUserByUsername(String name) throws UsernameNotFoundException {
         
-    	com.example.demo.object.User login_user = service.findUser(username);//idを引数に取れているか？
-    	
+    	com.example.demo.object.User login_user = service.findUser(name);
+    	List<GrantedAuthority>authorities = new ArrayList<>();
+        //DBにユーザー情報が登録されていなかった場合例外出力
     	if (login_user == null) {
-    		 throw new UsernameNotFoundException("user not found");
-        }
+   		 throw new UsernameNotFoundException("user not found");
+       }
+    	//user.dettails.user 型変数にコンストラクタの戻り値を格納 
+    	//user.dettails.user 型クラス以外キャスト出来ない？
+    	User Login_user = new User(login_user.getName(),login_user.getPassword(),authorities);
+    	UserDetails userDetails = (UserDetails)  Login_user;//認証できる形に変換
     	
-    	List<GrantedAuthority> authority = new ArrayList<>();
-    	
-    	UserDetails userDetails = (UserDetails) new User(login_user.getName(),login_user.getPassword(),authority);
-       
-    	return userDetails;
+    	return userDetails;//認証元に返す
     }
 }
