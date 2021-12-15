33388db77bbabddd1622ac38817b6e2cde3d9eee
Yuya Nagasaka <nagasaka.yuya0928@gmail.com>
2021-12-16 02:50:42 +0900

    [Add]バリデーションチェック機能の追記

--- src/main/java/com/example/demo/controller/CreateController.java
+++ src/main/java/com/example/demo/controller/CreateController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +12,8 @@ import com.example.demo.object.Output;
 import com.example.demo.service.OutputService;
 
 @Controller
+
+
 public class CreateController {
   
   @GetMapping("/Create")
@@ -20,7 +24,11 @@ public class CreateController {
   @Autowired OutputService service;
   
   @PostMapping("/Create")
-  public String PostRequest(@ModelAttribute Output  output) {
+  public String PostRequest(@ModelAttribute @Validated Output output,BindingResult bindingResult) {
+	 //Binding Result はModelに格納される
+	if(bindingResult.hasErrors()){
+		return "Create";
+	}
 	service.Create(output);
     return "redirect:/Index"; 
   }
--- src/main/java/com/example/demo/object/Output.java
+++ src/main/java/com/example/demo/object/Output.java
@@ -1,12 +1,22 @@
 package com.example.demo.object;
 
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Length;
+
 import lombok.Data;
 
 @Data
 public class Output {
   private int id;
   private int user_id;
+  @NotBlank
+  @Length(min = 1,max = 20)
   private String title;
+  @NotBlank
+  @Length(min = 1,max = 50)
   private String description;
+  @NotBlank
+  @Length(min = 1,max = 20)
   private String keyword;
 }
