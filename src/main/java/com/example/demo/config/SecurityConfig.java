74858ca1c64e79f580d1351a922329f8937c9a29
Yuya Nagasaka <nagasaka.yuya0928@gmail.com>
2021-12-17 19:36:41 +0900

    [ADD]静的ファイルの読み込み許可の追記

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -22,6 +23,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 	
 	@Autowired UserDetailsService userDetailsService;
+	
+	 @Override
+	    public void configure(WebSecurity web) throws Exception {
+	      web
+	        .ignoring()
+	          .antMatchers("/webjars/**")
+	          .antMatchers("/css/**")
+	          .antMatchers("/js/**");
+	 }
+	
 	
     @Override
     protected void configure(HttpSecurity http) throws Exception {
